46f8d0d085368738df20aae704bf8905c02364af
Denis <Buridza0@gmail.com>
2018-01-16 19:36:04 +0300

    new sout("return")

--- src/java/Main.java
+++ src/java/Main.java
@@ -3,5 +3,6 @@ package java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Goodbye, world!!!!");
+        System.out.println("i am return!");
     }
 }
